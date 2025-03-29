d3eba25c6158654b5227fa498a936e9c342ef52b
LittleEtx <1968717506@qq.com>
2025-03-29 17:22:28 +0800

    放置背景地图

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -29,7 +28,6 @@ kotlin {
     }
     
     sourceSets {
-        
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
@@ -37,12 +35,14 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.maplibre.compose)
+            implementation(libs.maplibre.compose.material3)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/littleetx/sustechnav/App.kt
+++ composeApp/src/commonMain/kotlin/com/littleetx/sustechnav/App.kt
@@ -1,36 +1,59 @@
 package com.littleetx.sustechnav
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import dev.sargunv.maplibrecompose.compose.MaplibreMap
+import dev.sargunv.maplibrecompose.compose.rememberCameraState
+import dev.sargunv.maplibrecompose.compose.rememberStyleState
+import dev.sargunv.maplibrecompose.core.CameraPosition
+import dev.sargunv.maplibrecompose.core.OrnamentSettings
+import dev.sargunv.maplibrecompose.material3.controls.AttributionButton
+import dev.sargunv.maplibrecompose.material3.controls.DisappearingCompassButton
+import dev.sargunv.maplibrecompose.material3.controls.DisappearingScaleBar
+import dev.sargunv.maplibrecompose.material3.controls.ScaleBarMeasure
+import dev.sargunv.maplibrecompose.material3.controls.ScaleBarMeasures
+import io.github.dellisd.spatialk.geojson.Position
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import sustechnav.composeapp.generated.resources.Res
-import sustechnav.composeapp.generated.resources.compose_multiplatform
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
+    val cameraState = rememberCameraState(
+        firstPosition = CameraPosition(
+            target = Position(latitude = 22.602, longitude = 113.995),
+            zoom = 14.5,
+        )
+    )
+
+    val styleState = rememberStyleState()
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        Box(modifier = Modifier.fillMaxSize()) {
+            MaplibreMap(
+                cameraState = cameraState,
+                styleState = styleState,
+                styleUri = Res.getUri("files/osm_liberty.json"),
+                ornamentSettings = OrnamentSettings.AllDisabled,
+            )
+
+            Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+                DisappearingScaleBar(
+                    measures = ScaleBarMeasures(primary = ScaleBarMeasure.Metric),
+                    metersPerDp = cameraState.metersPerDpAtTarget,
+                    zoom = cameraState.position.zoom,
+                    modifier = Modifier.align(Alignment.TopStart),
+                )
+                DisappearingCompassButton(cameraState, modifier = Modifier.align(Alignment.TopEnd))
+                AttributionButton(styleState, modifier = Modifier.align(Alignment.BottomEnd))
             }
         }
     }
